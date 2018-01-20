e982112b159823155d9dd0001121c77636f62411
boats <boats@mozilla.com>
2018-01-19 16:28:17 -0800

    Update dependencies.

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,21 +1,16 @@
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::time::Duration;
 
-use log::LevelFilter;
-
 #[derive(Configure, Deserialize)]
 #[configure(generate_docs)]
 #[serde(default)]
 pub struct Config {
-    #[configure(docs = "The address that your server will listen on. localhost:7878 by default.")]
+    /// The address that your server will listen on (localhost:7878 by default).
     pub addr: SocketAddr,
-    #[configure(docs = "Logging level for tyger (INFO by default).")]
-    pub log_level: LevelFilter,
-    #[configure(docs = "The maximum timeout that will be applied to every request (no timeout by \
-                        default).")]
+    /// The maximum timeout that will be applied to every request (no timeout by default).
     pub timeout: Option<Duration>,
-    #[configure(docs = "The address that the command/control server will listen on. If not set, \
-                        the command server will not be run.")]
+    /// The address that the command/control server will listen on. If not set, the command server
+    /// will not be run.
     pub cmd_addr: Option<SocketAddr>,
 }
 
@@ -23,7 +18,6 @@ impl Default for Config {
     fn default() -> Config {
         Config {
             addr: ("localhost", 7878).to_socket_addrs().unwrap().next().unwrap(),
-            log_level: LevelFilter::Info,
             timeout: None,
             cmd_addr: None,
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
 pub extern crate http;
 
 extern crate bytes;
-extern crate configure;
 extern crate failure;
 extern crate futures;
 extern crate hyper;
-extern crate log;
 extern crate serde;
 extern crate tokio_core;
 
-#[macro_use] extern crate configure_derive;
+#[macro_use] extern crate configure;
 #[macro_use] extern crate serde_derive;
 
 mod cfg;
